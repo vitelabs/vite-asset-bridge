f43972fe299457d98374d542b3c928ce422536c0
viteshan <wangjie@vite.org>
2020-11-11 10:49:31 +0800

    update unit test

--- test/ethblocks.test.js
+++ test/ethblocks.test.js
@@ -43,93 +43,93 @@ describe('eth block proof', () => {
             console.log(height+i, blockHex);
         }
     });
-    // it('block hash verify', async () => {
-    //     try {
+    it('block hash verify', async () => {
+        try {
 
-    //         let blockHash = '0x4a419022aa83efd6332ac4a0a0b5be84591a025e73a33086fe918b03bc11de41';
-    //         const block = await web3.eth.getBlock(blockHash);
-    //         console.log('block', JSON.stringify(block));
-    //         console.log('block hex', bufferToHex(Header.fromRpc(block).serialize()));
-    //         console.log(keccak256(Header.fromRpc(block).serialize()).toString('hex'));
-    //         const blockHeader = Header.fromRpc(block)
-    //         const nestedList = [
-    //             block.parentHash,
-    //             block.sha3Uncles,
-    //             block.miner,
-    //             block.stateRoot,
-    //             block.transactionsRoot,
-    //             block.receiptsRoot,
-    //             block.logsBloom,
-    //             Number(block.difficulty),
-    //             Number(block.number),
-    //             Number(block.gasLimit),
-    //             Number(block.gasUsed),
-    //             Number(block.timestamp),
-    //             block.extraData,
-    //             block.mixHash,
-    //             block.nonce
-    //         ];
-    //         console.log("difficulty 1", RLP.encode(block.difficulty));
-    //         console.log("difficulty 1", block.difficulty);
-    //         console.log("difficulty 1", Number(block.difficulty));
-    //         console.log("difficulty 1", Number(block.difficulty).toString(16));
-    //         console.log("difficulty 2", bufferToHex(Header.fromRpc(block).difficulty));
-    //         const encoded = RLP.encode(nestedList);
-    //         console.log("hex block hash", blockHash);
-    //         console.log("hex block", bufferToHex(encoded));
-    //         assert.equal('0x' + keccak256(encoded).toString('hex'), blockHash);
-    //     } catch (e) {
-    //         console.error(e);
-    //     }
-    // });
+            let blockHash = '0x4a419022aa83efd6332ac4a0a0b5be84591a025e73a33086fe918b03bc11de41';
+            const block = await web3.eth.getBlock(blockHash);
+            console.log('block', JSON.stringify(block));
+            console.log('block hex', bufferToHex(Header.fromRpc(block).serialize()));
+            console.log(keccak256(Header.fromRpc(block).serialize()).toString('hex'));
+            const blockHeader = Header.fromRpc(block)
+            const nestedList = [
+                block.parentHash,
+                block.sha3Uncles,
+                block.miner,
+                block.stateRoot,
+                block.transactionsRoot,
+                block.receiptsRoot,
+                block.logsBloom,
+                Number(block.difficulty),
+                Number(block.number),
+                Number(block.gasLimit),
+                Number(block.gasUsed),
+                Number(block.timestamp),
+                block.extraData,
+                block.mixHash,
+                block.nonce
+            ];
+            console.log("difficulty 1", RLP.encode(block.difficulty));
+            console.log("difficulty 1", block.difficulty);
+            console.log("difficulty 1", Number(block.difficulty));
+            console.log("difficulty 1", Number(block.difficulty).toString(16));
+            console.log("difficulty 2", bufferToHex(Header.fromRpc(block).difficulty));
+            const encoded = RLP.encode(nestedList);
+            console.log("hex block hash", blockHash);
+            console.log("hex block", bufferToHex(encoded));
+            assert.equal('0x' + keccak256(encoded).toString('hex'), blockHash);
+        } catch (e) {
+            console.error(e);
+        }
+    });
 
-    // it('receipt hash verify', async () => {
-    //     try {
-    //         // const tx = await web3.eth.getTransaction('0x92e94b3ac35eb7a34fde6d4b24337f7c6db2ede68e2b564b81ff4b0fe4d7a8f3');
-    //         // console.log(tx);
+    it('receipt hash verify', async () => {
+        try {
+            // const tx = await web3.eth.getTransaction('0x92e94b3ac35eb7a34fde6d4b24337f7c6db2ede68e2b564b81ff4b0fe4d7a8f3');
+            // console.log(tx);
 
-    //         // const receipt = await web3.eth.getTransactionReceipt('0x92e94b3ac35eb7a34fde6d4b24337f7c6db2ede68e2b564b81ff4b0fe4d7a8f3');
-    //         // console.log(JSON.stringify(receipt));
+            // const receipt = await web3.eth.getTransactionReceipt('0x92e94b3ac35eb7a34fde6d4b24337f7c6db2ede68e2b564b81ff4b0fe4d7a8f3');
+            // console.log(JSON.stringify(receipt));
 
-    //         let blockHash = '0x4a419022aa83efd6332ac4a0a0b5be84591a025e73a33086fe918b03bc11de41';
-    //         const block = await web3.eth.getBlock(blockHash);
-    //         let receipts = await Promise.all(block.transactions.map((txHash) => {
-    //             return web3.eth.getTransactionReceipt(txHash)
-    //         }));
+            let blockHash = '0x4a419022aa83efd6332ac4a0a0b5be84591a025e73a33086fe918b03bc11de41';
+            const block = await web3.eth.getBlock(blockHash);
+            let receipts = await Promise.all(block.transactions.map((txHash) => {
+                return web3.eth.getTransactionReceipt(txHash)
+            }));
 
-    //         let trie = new Trie();
-    //         await Promise.all(receipts.map((receipt) => {
-    //             receipt.status = receipt.status ? 1 : 0;
-    //             let logs = []
-    //             for (let i = 0; i < receipt.logs.length; i++) {
-    //                 logs.push(
-    //                     [
-    //                         receipt.logs[i].address,
-    //                         receipt.logs[i].topics,
-    //                         receipt.logs[i].data
-    //                     ]
-    //                 )
-    //             }
-    //             const raw = RLP.encode([
-    //                 toBuffer(receipt.status || undefined),
-    //                 toBuffer(receipt.cumulativeGasUsed),
-    //                 receipt.logsBloom,
-    //                 logs
-    //             ]);
-    //             const serialize = Receipt.fromRpc(receipt).serialize();
-    //             assert.equal(bufferToHex(raw), bufferToHex(serialize));
-    //             return trie.put(encode(receipt.transactionIndex), raw)
-    //         }));
-    //         assert.equal(bufferToHex(trie.root), block.receiptsRoot);
+            let trie = new Trie();
+            await Promise.all(receipts.map((receipt) => {
+                receipt.status = receipt.status ? 1 : 0;
+                let logs = []
+                for (let i = 0; i < receipt.logs.length; i++) {
+                    logs.push(
+                        [
+                            receipt.logs[i].address,
+                            receipt.logs[i].topics,
+                            receipt.logs[i].data
+                        ]
+                    )
+                }
+                const raw = RLP.encode([
+                    toBuffer(receipt.status || undefined),
+                    toBuffer(receipt.cumulativeGasUsed),
+                    receipt.logsBloom,
+                    logs
+                ]);
+                const serialize = Receipt.fromRpc(receipt).serialize();
+                assert.equal(bufferToHex(raw), bufferToHex(serialize));
+                return trie.put(encode(receipt.transactionIndex), raw)
+            }));
+            assert.equal(bufferToHex(trie.root), block.receiptsRoot);
 
-    //         for (let i = 0; i < receipts.length; i++) {
-    //             const proofs = await Trie.createProof(trie, encode(i));
-    //             const key = await Trie.verifyProof(trie.root, encode(i), proofs)
-    //             assert.equal(bufferToHex(key), bufferToHex(Receipt.fromRpc(receipts[i]).serialize()));
-    //         }
-    //     } catch (e) {
-    //         console.error(e);
-    //         assert.fail();
-    //     }
-    // });
+            for (let i = 0; i < receipts.length; i++) {
+                const proofs = await Trie.createProof(trie, encode(i));
+                const key = await Trie.verifyProof(trie.root, encode(i), proofs)
+                assert.equal(bufferToHex(key), bufferToHex(Receipt.fromRpc(receipts[i]).serialize()));
+            }
+        } catch (e) {
+            console.error(e);
+            assert.fail();
+        }
+    });
 });
