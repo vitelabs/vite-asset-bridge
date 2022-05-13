e283ca9345a4ea3572da9073106744bb984ef174
LeonardoBill <libi@vite.org>
2022-05-13 21:33:51 +0800

    update: add channel input

---
+++ bridge-vite/scripts_deploy/3.0input_with_test.ts
@@ -0,0 +1,33 @@
+import { expect } from "chai";
+const vuilder = require("@vite/vuilder");
+import config from "./deploy.config.json";
+import inputCfg from "./input.config.json";
+import vaultCfg from "./contract.config.json";
+
+async function run(): Promise<void> {
+  const provider = vuilder.newProvider(config.http);
+  const deployer = vuilder.newAccount(config.deployer, 0, provider);
+
+  const compiledContracts = await vuilder.compile("Vault.solpp");
+  expect(compiledContracts).to.have.property("Vault");
+
+  // deploy
+  const vault = compiledContracts.Vault;
+  vault.setDeployer(deployer).setProvider(provider);
+  await vault.attach(vaultCfg.vault);
+  expect(vault.address).to.be.a("string");
+
+  //   console.log(deployer.address, vault.address, vaultCfg.channelId, inputCfg.dest, inputCfg.value, vault.abi);
+  const block = await vault.call(
+    "input",
+    [vaultCfg.channelId, inputCfg.dest, inputCfg.value],
+    { amount: inputCfg.value }
+  );
+
+  console.log("result: ", JSON.stringify({ success: true }));
+  return;
+}
+
+run().then(() => {
+  console.log("done");
+});
